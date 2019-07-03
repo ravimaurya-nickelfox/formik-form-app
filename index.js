291b2c577f4ca86f3debf273bf7d8cf1b01ff64b
Ravi Kumar Maurya <ravi.maurya@nickelfox.com>
2019-07-03 15:38:45 +0530

    removed unused imports from index.js

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 import {AppRegistry} from 'react-native';
-import App from './App';
 import {name as appName} from './app.json';
-import Form from './Form';
-import Twillio from './Twillio';
-import TwilioVideos from './TwilioVideos';
-import AnimationView from './AnimationView';
-import SlideShow from './SlideShow';
 import Main from './Main';
-
 
 AppRegistry.registerComponent(appName, () =>Main);
