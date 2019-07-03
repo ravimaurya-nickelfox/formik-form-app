d71301b33e6815d025cff0a9b57c316b1322dd0b
Ravi Kumar Maurya <ravi.maurya@nickelfox.com>
2019-07-03 12:46:38 +0530

    deeplinking receive params

--- DeepLinkedScreen.js
+++ DeepLinkedScreen.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 
 export default class DeepLinkedScreen extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            params:this.props.navigation.state.params
+        }
+        console.log()
+    }
     render() {
         return (
             <View>
-                <Text> textInComponent </Text>
+                <Text> You are viewing appointment for id 
+                    <Text style={{fontWeight:'600',textDecorationLine:'underline'}}> {this.state.params.appointment_id}</Text>
+                </Text>
             </View>
         )
     }
