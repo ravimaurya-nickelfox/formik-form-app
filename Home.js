250f6fa845d4d01652509815bcde7e82d32db8a7
Ravi Kumar Maurya <ravi.maurya@nickelfox.com>
2019-07-04 18:22:48 +0530

    refactor

--- Home.js
+++ Home.js
@@ -16,22 +16,22 @@ export default class Home extends Component {
     render() {
         return (
             <SafeAreaView style={Styles.container}>
-                <TouchableOpacity style={Styles.button} onPress={()=>this.btnPressAction('signup')}>
+                <TouchableOpacity activeOpacity={0.7} style={Styles.button} onPress={()=>this.btnPressAction('signup')}>
                     <Text style={Styles.btnTxt}>Sign Up Form</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={Styles.button} onPress={()=>this.btnPressAction('tvideo')}>
+                <TouchableOpacity activeOpacity={0.7} style={Styles.button} onPress={()=>this.btnPressAction('tvideo')}>
                     <Text style={Styles.btnTxt}>Twilio Video Chat</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={Styles.button} onPress={()=>this.btnPressAction('slides')}>
+                <TouchableOpacity activeOpacity={0.7} style={Styles.button} onPress={()=>this.btnPressAction('slides')}>
                     <Text style={Styles.btnTxt}>Slide Animation</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={Styles.button} onPress={()=>this.btnPressAction('deeplinked')}>
+                <TouchableOpacity activeOpacity={0.7} style={Styles.button} onPress={()=>this.btnPressAction('deeplinked')}>
                     <Text style={Styles.btnTxt}>Deep Linked Screen</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={Styles.button} onPress={()=>this.btnPressAction('form')}>
+                <TouchableOpacity activeOpacity={0.7} style={Styles.button} onPress={()=>this.btnPressAction('form')}>
                     <Text style={Styles.btnTxt}>Forms</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={Styles.button} onPress={()=>this.btnPressAction('analytics')}>
+                <TouchableOpacity activeOpacity={0.7} style={Styles.button} onPress={()=>this.btnPressAction('analytics')}>
                     <Text style={Styles.btnTxt}>Analytics</Text>
                 </TouchableOpacity>
             </SafeAreaView>
@@ -46,7 +46,7 @@ const Styles = StyleSheet.create({
         alignItems:'center'
     },
     button:{
-        backgroundColor:'#d3d3d3',
+        backgroundColor:'rgb(37,134,227)',
         paddingHorizontal:30,
         paddingVertical:15,
         borderRadius:4,
